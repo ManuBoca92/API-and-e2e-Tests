22c1a7f5019bfedd94e02924d5815f85f359550e
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-09-20 21:39:44 +0200

    - ui automation tests using protractor

---
+++ ui_test_protractor/test-spec.js
@@ -0,0 +1,11 @@
+import { element, by } from "protractor";
+
+describe("Allianz ui test", function() {
+  it("should add a todo", function() {
+    browser.waitForAngularEnabled(false);
+    browser.get(
+      "https://b2c-es.uat.booking.allianz-assistance.com/iframe.html?angularparams=/TRAVEL/B2C/ES/es_ES/step-1"
+    );
+    browser.switchTo().frame(element(by.tagName("iframe")).getWebElement());
+  });
+});
